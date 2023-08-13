eb755703bd55fde90067a44ecb8a9333f597fbbc
ArtemLevin <python_study_2022@mail.ru>
2023-08-14 00:55:44 +0300

    counting recursion depth

--- decoratorsBasics.py
+++ decoratorsBasics.py
@@ -1,20 +1,48 @@
-def memoization(func):
-    valueDict = {}
-    def wrapper(arg):
-        nonlocaL = valueDict
-        if arg in valueDict.keys():
-            return valueDict[arg]
-        else:
-            valueDict[arg] = func(arg)
-            return valueDict[arg]
+from collections import OrderedDict
+from functools import wraps
+
+
+def wrapped(function):
+    @wraps(function)
+    def inner(arg):
+        return function(arg)
+
+    return inner
+
+
+def greater_than_max_length(value):
+    def predicate(arg):
+        return arg >= value
+
+    return predicate
+
+
+def memoization(predicate):
+    valueDict = OrderedDict()
+
+    def wrapper(func):
+        def inner(arg):
+            nonlocaL = valueDict
+            if arg in valueDict.keys():
+                return valueDict[arg]
+            else:
+                if predicate(len(valueDict)):
+                    del valueDict[list(valueDict)[0]]
+                valueDict[arg] = func(arg)
+                return valueDict[arg]
+
+        return inner
+
     return wrapper
 
-@memoization
+
+@wrapped
+@memoization(greater_than_max_length(10))
 def fib(n):
-    if n == 1 or n == 2: return 1
-    else: return fib(n-1) + fib(n-2)
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
 
 
-print(fib(50))
-
-
+print(fib(100))
--- experiments.py
+++ experiments.py
@@ -1,26 +1,19 @@
-# def greet(name, surname):
-#     return f'Hello, {name} {surname}!'
-#
-# def partial_aplly(someFunc, argForSomeFunc):
-#     def getAnotherArg(anotherArg):
-#         return someFunc(argForSomeFunc, anotherArg)
-#     return getAnotherArg
-#
-# f = partial_aplly(greet, 'Dorian')
-# print(f('Grey'))
+def counter(func):
+    count = 0
+    def wrapper(arg):
+        nonlocal count
+        count +=1
+        print(f'{count = }')
+        return func(arg)
+
+    return wrapper
+
+@counter
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 
-# def partial_apply(function, arg1):
-#     def inner(arg2):
-#         return function(arg1, arg2)
-#     return inner
-
-def func(arg1, arg2):
-    return f'{arg1 = }, {arg2 =}'
-def flip(func):
-    def inner(arg1, arg2):
-        return func(arg2, arg1)
-    return inner
-
-c = flip(func)
-print(c(1, 2))
+print(fib(7))
